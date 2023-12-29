a45934f771350858ae7fe26868d100b4147d45cd
BackiaRaj <backiaraj.kanagaraj@syncfusion.com>
2023-12-29 16:39:11 +0530

    Update the project

---
+++ Data/Offers.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Threading.Tasks;
+
+namespace RefreshDataAsyncSample.Data
+{
+    public class EmployeeData
+    {
+        public int? Id { get; set; }
+        public EmployeeName Name { get; set; }
+        public string Title { get; set; }
+    }
+
+    public class EmployeeName
+    {
+        public int FirstName { get; set; }
+        public Emp LastName { get; set; }
+        public string Text { get; set; }
+    }
+
+    public class Emp
+    {
+        public int Id { get; set; }
+        public string Nam { get; set; }
+    }
+}
---
+++ Data/OrderContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using RefreshDataAsyncSample.Data;
+
+namespace RefreshDataAsyncSample.Data
+{
+    public class OrderContext : DbContext
+    {
+        public virtual DbSet<Order> Orders { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=D:\GitHub\SyncfusionExamples\MultiSelectDropDown\refresh-data-async\multi1\RefreshDataAsyncSample\App_Data\NORTHWND.MDF;Integrated Security=True");
+            }
+        }
+    }
+ 
+}
